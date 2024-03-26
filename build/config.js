6c039f5b93f67ae107d468298da5612939229361
ZTCodeCraft <ztcodecraft@gmail.com>
2024-03-26 14:03:53 +0200

    remove unnecessary files

--- build/config.js
+++ build/config.js
@@ -4,13 +4,7 @@ const path = {
   src_js: 'src/js',
   js: 'assets/js',
   css: 'assets/css',
-  vendor: 'assets/vendor',
-  icons: {
-    src: 'src/icons',
-    output: 'assets/icons',
-    fontName: 'around-icons',
-    cssPrefix: 'ai',
-  },
+  vendor: 'assets/vendor'
 }
 
 module.exports = path
